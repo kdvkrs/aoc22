5592934fd43b84d6ef9963a0e7827a9fa3c27192
Klaus Kraßnitzer <klaus.krassnitzer@tuwien.ac.at>
2022-12-23 21:32:08 +0100

    day23: tidy up

--- day23/part1.py
+++ day23/part1.py
@@ -24,12 +24,9 @@ def bbox(elves):
 def plot(elves):
     if VERBOSE:
         rmin, rmax, cmin, cmax = bbox(elves)
-        rmin -= 1
-        cmin -= 1
-        rmax += 1
-        cmax += 1
+        rmin -= 1; cmin -= 1; rmax += 1; cmax += 1
         plt = [["." for _ in range(cmax-cmin)] for _ in range(rmax-rmin)]
-        debug(rmin, rmax, cmin, cmax)
+        debug(f"Plotting: {rmin, cmin}->{rmax, cmax}")
         for er, ec in elves:
             plt[er-rmin][ec-cmin] = "#"
         print("\n".join(["".join(p) for p in plt]))
@@ -50,6 +47,7 @@ def main(part=1):
                 return False
         return True
 
+    debug("Initial State")
     plot(elves)
 
     stay = set()     
@@ -58,7 +56,6 @@ def main(part=1):
 
     def add(p, op):
         if p in fe:
-            #debug("clash in pos", p, "op", op)
             no_move.add(p) 
             stay.add(fe[p])
             del fe[p]
@@ -66,49 +63,34 @@ def main(part=1):
             fe[p] = op
         else:
             stay.add(op)
-
-    def north(e):
-        er, ec = e
-        if free([(er-1, ec-1), (er-1, ec), (er-1, ec+1)]):
-            # debug("move north")
-            add((er-1, ec), (er, ec))
-            return True
-        return False
     
-    def south(e):
-        er, ec = e
-        if free([(er+1, ec-1), (er+1, ec), (er+1, ec+1)]):
-            # debug("move south")
-            add((er+1, ec), (er, ec))
-            return True
-        return False
+    def direction(e, off):
+        er, ec = e; 
+        offr, offc = off
+        if offr == 0:
+            cond = free([(er-1, ec+offc), (er, ec+offc), (er+1, ec+offc)])
+        else:
+            cond = free([(er+offr, ec-1), (er+offr, ec), (er+offr, ec+1)])
+        if cond:
+            add((er+offr, ec+offc), e)
+        return cond
 
-    def west(e):
-        er, ec = e
-        if free([(er+1, ec-1), (er, ec-1), (er-1, ec-1)]):
-            # debug("move west")
-            add((er, ec-1), (er, ec))
-            return True
-        return False
-    
-    def east(e):
-        er, ec = e
-        if free([(er+1, ec+1), (er, ec+1), (er-1, ec+1)]):
-            # debug("move east")
-            add((er, ec+1), (er, ec))
-            return True
-        return False
+
+    def north(e): return direction(e, (-1, 0))
+    def south(e): return direction(e, (1, 0))
+    def west(e): return direction(e, (0, -1))
+    def east(e): return direction(e, (0, 1))
 
     q = deque([north, south, west, east])
-    round = 0
+    rnd = 0
     while True:
-        round += 1
+        rnd += 1
+        debug("\nRound", rnd)
         stay.clear()
         no_move.clear()
         fe.clear()
         term = True
         for er,ec in elves:
-            # debug("ELF", er,ec)
             if free([(er+1, ec+1), (er, ec+1), (er-1, ec+1), (er+1, ec-1), (er, ec-1), (er-1, ec-1), (er-1, ec), (er+1, ec)]):
                 stay.add((er,ec))
             else:
@@ -120,18 +102,22 @@ def main(part=1):
                 if not success:
                     stay.add((er,ec))
                 term = False
-        if term or part==1 and round==11:
-            break
-        q.rotate(-1)
-
         elves = set(fe.keys()).union(stay)
         plot(elves)
+
+        if term or part==1 and rnd==10:
+            debug("Terminate")
+            break
+
+        # turn directions
+        q.rotate(-1)
+
 
     brs, bre, bcs, bce = bbox(elves)
     if part==1:
         print((bre-brs)*(bce-bcs)-len(elves))
     else:
-        print(round)
+        print(rnd)
 
 if __name__ == "__main__":
     main()
